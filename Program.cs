bfbea680b9539987571d9377edc4fdf32df7b62e
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2024-08-30 13:36:11 +0200

    Gjorde uppgifterna 1, 2 och 3

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Är det fint väder idag?");
+string svar = Console.ReadLine();
+if (svar == "j" || svar == "J") {
+    Console.WriteLine("Vi går på piknik!");
+} else if (svar == "n" || svar == "N") {
+    Console.WriteLine("Vi stanar inne och läser en bok.");
+} else {
+    Console.WriteLine("Jag förstår inte.");
+}
